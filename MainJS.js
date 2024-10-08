ceb8a3e3b715952dfaa4d9e0abdac22acd73cc2d
AhmadM51 <ahmadmustafawi@Ahmacs-MacBook-Pro.local>
2024-10-08 20:35:57 +0400

    added diffrent drop down menus

--- MainJS.js
+++ MainJS.js
@@ -1,4 +1,9 @@
 let themeMood = 'light';
+let dropdown_1 = false;
+let dropdown_2 = false;
+let dropdown_3 = false;
+let dropdown_4 = false;
+
 function changeTheme(){
     if(themeMood == 'light'){
         document.getElementById('bodyID').className='Darkmode';
@@ -10,4 +15,49 @@ function changeTheme(){
         document.getElementById('darkModeBtn').className='Darkmode';
         themeMood = 'light';
     }   
+}
+function Eddropdowninf(elementid){
+    switch (elementid) {
+
+       case 1: {
+            if(dropdown_1 == false){
+                document.querySelector('#eduDropDown').className='apear';
+                dropdown_1 = true;
+            }else{
+                document.querySelector('#eduDropDown').className='dissapear';
+                dropdown_1 = false;
+            }
+            break;
+        }
+        case 2: {
+            if(dropdown_2 == false){
+                document.querySelector('#skillDropDown').className='apear';
+                dropdown_2 = true;
+            }else{
+                document.querySelector('#skillDropDown').className='dissapear';
+                dropdown_2 = false;
+            }
+            break;
+        }
+        case 3: {
+            if(dropdown_3 == false){
+                document.querySelector('#achDropDown').className='apear';
+                dropdown_3 = true;
+            }else{
+                document.querySelector('#achDropDown').className='dissapear';
+                dropdown_3 = false;
+            }
+            break;
+        }
+        case 4: {
+            if(dropdown_4 == false){
+                document.querySelector('#hobDropDown').className='apear';
+                dropdown_4 = true;
+            }else{
+                document.querySelector('#hobDropDown').className='dissapear';
+                dropdown_4 = false;
+            }
+            break;
+        }
+    }
 }
